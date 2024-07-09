106365725773aa91510e7d8b376491965802520a
2022051615301liuyan <2627369544@qq.com>
2024-07-09 18:37:41 +0800

    修改遗漏4

--- tcpclient.h
+++ tcpclient.h
@@ -18,16 +18,17 @@
 #define PORT1 8888
 #define MaxClient 20
 
-class tcpClient : public QObject
+class TcpClient : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
 public:
-    explicit tcpClient(QObject *parent = nullptr);
+    explicit TcpClient(QObject *parent = nullptr);
 
 public:
     //判断传输文件的类型的函数
-    Q_INVOKABLE int filetype(QString file_path); //注意！qml中调用此函数即可发送文件夹或文件
+    Q_INVOKABLE void switchflag(QString file_path);
+    //注意！qml中调用此函数即可发送文件夹或文件
 
 private:
     //不同文件传输类型的函数
@@ -35,7 +36,7 @@ private:
     Q_INVOKABLE void filetransmitC(QString file_path);   //文件类型发送
     //判断传输文件的类型的函数
     Q_INVOKABLE int switchfile(QString file_path);
-    Q_INVOKABLE void switchflag(QString file_path);
+    Q_INVOKABLE int filetype(QString file_path);
 
     int _socketfd;
     sockaddr_in *_addr;
--- tcpserver.h
+++ tcpserver.h
@@ -18,23 +18,24 @@
 #define PORT2 8887
 #define MaxClient 20
 
-class tcpSeverer : public QObject
+class TcpSeverer : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
 public:
-    explicit tcpSeverer(QObject *parent = nullptr);
+    explicit TcpSeverer(QObject *parent = nullptr);
     //不同类型的文件传输
 public:
     //判断传输文件的类型的函数
-    Q_INVOKABLE int filetype(QString file_path); //注意！qml中调用此函数即可发送文件夹或文件
+    Q_INVOKABLE void switchflag(QString file_path);
+    //注意！qml中调用此函数即可接收文件夹或文件
 
 private:
     Q_INVOKABLE void foldertransmitS(QString file_path); //文件夹类型接收
     Q_INVOKABLE void filetransmitS(QString file_path);   //文件类型接收
     //判断传输文件的类型的函数
     Q_INVOKABLE int switchfile(QString file_path);
-    Q_INVOKABLE void switchflag(QString file_path);
+    Q_INVOKABLE int filetype(QString file_path);
 
     int _socketfd;
     sockaddr_in *_addr;
